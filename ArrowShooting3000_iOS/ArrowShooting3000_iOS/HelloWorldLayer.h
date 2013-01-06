09b1fb59741ad9d01e64b96f77203bf8cf3ee0b7
bgotthart <bianca.gotthart@gmail.com>
2013-01-06 20:19:22 +0100

    add new bigger arrow

--- ArrowShooting3000_iOS/ArrowShooting3000_iOS/HelloWorldLayer.h
+++ ArrowShooting3000_iOS/ArrowShooting3000_iOS/HelloWorldLayer.h
@@ -19,6 +19,9 @@
 #import "arrowHud.h"
 #import "player.h"
 
+#import "LIImageView.h"
+#import "LIAnimationManagerView.h"
+
 //Pixel to metres ratio. Box2D uses metres as the unit for measurement.
 //This ratio defines how many pixels correspond to 1 Box2D "metre"
 //Box2D is optimized for objects of 1x1 metre therefore it makes sense
